4fc05b3450054fa38e57c4b9194aed4876f4dd50
Numantic-NMoroney <nathan.moroney@numanticsolutions.com>
2025-02-18 13:24:44 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -2,8 +2,30 @@
 #
 
 import streamlit as st
+import psycopg2
+from dotenv import load_dotenv
+import os
 
 
 st.subheader("Test App")
 st.text('GCP + Postgres + Cloud Run + Streamlit.')
+
+load_dotenv()
+conn_str = ( "host="      + os.getenv('GRES_HOST') + " " +
+             "port="      + os.getenv('GRES_PORT') + " " +
+             "dbname="    + os.getenv('GRES_DB')   + " " +
+             "user="      + os.getenv('GRES_USER') + " " +
+             "password="  + os.getenv('GRES_PWD') )
+
+conn = psycopg2.connect(conn_str)
+cur = conn.cursor()
+
+query = "SELECT * FROM c3pa_app_usage;"
+cur.execute(query)
+
+results = cur.fetchall()
+
+for result in results:
+    print(result)
+
 
