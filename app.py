434a3d5d53765d7ac0372da3a9bf128b8b2b10ea
Numantic-NMoroney <nathan.moroney@numanticsolutions.com>
2025-02-19 14:13:53 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -2,30 +2,37 @@
 #
 
 import streamlit as st
-import psycopg2
-from dotenv import load_dotenv
+# import psycopg2
+# from dotenv import load_dotenv
 import os
 
 
 st.subheader("Test App")
-st.text('GCP + Postgres + Cloud Run + Streamlit.')
+st.text('GCP + Cloud Storage + Cloud Run + Streamlit.')
 
-load_dotenv()
-conn_str = ( "host="      + os.getenv('PG_HOST') + " " +
-             "port="      + os.getenv('PG_PORT') + " " +
-             "dbname="    + os.getenv('PG_DB')   + " " +
-             "user="      + os.getenv('PG_USER') + " " +
-             "password="  + os.getenv('PG_PWD') )
+bucket_mount_path = os.environ.get('MOUNT_PATH', '/c3pa-app')
 
-conn = psycopg2.connect(conn_str)
-cur = conn.cursor()
+with open("app_counter.csv", "r") as file:
+    for line in file:
+        print(line.strip())
 
-query = "SELECT * FROM c3pa_app_usage;"
-cur.execute(query)
 
-results = cur.fetchall()
-
-for result in results:
-    print(result)
+# load_dotenv()
+# conn_str = ( "host="      + os.getenv('PG_HOST') + " " +
+#              "port="      + os.getenv('PG_PORT') + " " +
+#              "dbname="    + os.getenv('PG_DB')   + " " +
+#              "user="      + os.getenv('PG_USER') + " " +
+#              "password="  + os.getenv('PG_PWD') )
+# 
+# conn = psycopg2.connect(conn_str)
+# cur = conn.cursor()
+# 
+# query = "SELECT * FROM c3pa_app_usage;"
+# cur.execute(query)
+# 
+# results = cur.fetchall()
+# 
+# for result in results:
+#     print(result)
 
 
