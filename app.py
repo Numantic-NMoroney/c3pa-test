5695bc263e02f7b0188e48ff37313d322cd079a1
Numantic-NMoroney <nathan.moroney@numanticsolutions.com>
2025-02-20 10:29:32 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -2,37 +2,20 @@
 #
 
 import streamlit as st
-# import psycopg2
-# from dotenv import load_dotenv
 import os
 
 
 st.subheader("Test App")
-st.text('GCP + Cloud Storage + Cloud Run + Streamlit.')
+st.text('GCP + Cloud Storage Bucket + Cloud Run + Streamlit.')
 
 bucket_mount_path = os.environ.get('MOUNT_PATH', '/c3pa-app')
 
-with open("app_counter.csv", "r") as file:
+name_csv = 'app_counter.csv'
+file_path = os.path.join(bucket_mount_path, name_csv)
+
+st.text("bucket mount path : " + bucket_mount_path)
+
+with open(file_path, "r") as file:
     for line in file:
-        print(line.strip())
-
-
-# load_dotenv()
-# conn_str = ( "host="      + os.getenv('PG_HOST') + " " +
-#              "port="      + os.getenv('PG_PORT') + " " +
-#              "dbname="    + os.getenv('PG_DB')   + " " +
-#              "user="      + os.getenv('PG_USER') + " " +
-#              "password="  + os.getenv('PG_PWD') )
-# 
-# conn = psycopg2.connect(conn_str)
-# cur = conn.cursor()
-# 
-# query = "SELECT * FROM c3pa_app_usage;"
-# cur.execute(query)
-# 
-# results = cur.fetchall()
-# 
-# for result in results:
-#     print(result)
-
+        st.text(line.strip())
 
