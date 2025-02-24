cc041cf68452f2b370f7f1e6bf7e44bd3f3e4461
Numantic-NMoroney <nathan.moroney@numanticsolutions.com>
2025-02-24 09:36:47 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -5,39 +5,21 @@ import streamlit as st
 import os
 
 
-bucket_mount_path = os.environ.get('MOUNT_PATH', '/c3pa-app')
-# bucket_mount_path = ''
+# questions_path = os.environ.get('MOUNT_PATH', '/c3pa-app/testing')
+questions_path = 'c3pa-app/testing'
 
-name_csv = 'app_counter.csv'
-file_path = os.path.join(bucket_mount_path, name_csv)
-# file_path = name_csv
-
-
-def read_lines(file_path):
-    lines = []
-    with open(file_path, "r") as file:
-        for line in file:
-            lines.append(line.strip())
-    return lines
+name_txt = 'questions-2502-q26.txt'
+file_path = os.path.join(questions_path, name_txt)
 
 
-if 'lines' not in st.session_state:
-    st.session_state.lines = read_lines(file_path)
+st.subheader("C3PA Testing")
+st.text('Batch mode testing of the California Community Colleges Policy-Assistant.')
 
-st.subheader("Press the Button?")
-st.text('Please consider pressing the button.')
+st.divider()
 
-if st.button("Button", type="primary"):
-    with open(file_path, "w") as file:
-        n1 = int(st.session_state.lines[1]) + 1
-        st.session_state.lines[1] = str(n1)
-
-        file.write(st.session_state.lines[0] + "\n")
-        file.write(str(n1) + "\n")
-
-st.text("The button has been pressed " + 
-        str(st.session_state.lines[1]) + 
-        " times.")
-
-st.text("Thank you.")
+questions = []
+with open(file_path, "r") as file:
+    for line in file:
+        questions.append(line.strip())
+        st.text(line)
 
