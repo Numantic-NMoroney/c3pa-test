f9e79367e498ba10c83cee9429b2c973849d88d0
Numantic-NMoroney <nathan.moroney@numanticsolutions.com>
2025-02-27 10:11:05 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -23,7 +23,8 @@ testing_path = os.environ.get('MOUNT_PATH', '/c3pa-app/testing')    # gcs
 
 # name_txt = 'questions-250226-q10-a.txt'
 # name_txt = 'questions-250226-q10-b.txt'
-name_txt = 'questions-250226-q10-c.txt'
+# name_txt = 'questions-250226-q10-c.txt'
+name_txt = 'questions-250226-q10-d.txt'
 # name_txt = 'questions-2502-q26.txt'
 
 in_path = os.path.join(testing_path, name_txt)
